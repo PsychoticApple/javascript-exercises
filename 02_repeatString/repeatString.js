d5a81a536b60367fb1d6998bb056dbd3d5e5158d
Vinícius da Silva <vinicius.vinci132020@gmail.com>
2024-08-01 09:08:32 -0300

    Finish exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+  let str = "";
+  for (let i = 1; i <= num; i++) {
+    str += string;
+  }
+  return num < 0 ? "ERROR" : str;
 };
 
 // Do not edit below this line
